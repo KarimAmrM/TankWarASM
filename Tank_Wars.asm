3e77435c3f357d35feb0fcf97cc927548da785bd
moza-loza <61754531+moza-loza@users.noreply.github.com>
2020-12-27 01:16:25 +0200

    Draw Horizontal

--- Tank_Wars.asm
+++ Tank_Wars.asm
@@ -14,6 +14,22 @@ JNZ Draw
 
 ENDM DrawVerticalLine
 
+DrawHorizontalLine MACRO X,Y,L,Colour
+LOCAL Draw
+MOV AH,0CH
+MOV AL,Colour
+MOV CX,X
+MOV DX,Y
+MOV BX,X
+ADD BX,L
+
+Draw:INt 10h
+INC CX
+CMP CX,BX
+JNZ Draw
+
+ENDM DrawHorizontalLine
+
 .MODEL Medium
 .STACK 64
 .DATA
@@ -24,7 +40,7 @@ MOV AH,0
 MOV AL,13H
 int 10H
 
-DrawVerticalLine 15,0,200,0Bh
+DrawHorizontalLine 15,20,200,02h
 
 ;DrawVerticalLine 16,0,200,03h
 hlt
