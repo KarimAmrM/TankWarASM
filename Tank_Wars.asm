3238c8c3d95316e384f9a021a1ae3c300dbfaa64
moza-loza <61754531+moza-loza@users.noreply.github.com>
2020-12-27 01:59:04 +0200

    Draw Tank

--- Tank_Wars.asm
+++ Tank_Wars.asm
@@ -1,11 +1,12 @@
 DrawVerticalLine MACRO X,Y,L,Colour
 LOCAL Draw
-MOV AH,0CH
-MOV AL,Colour
 MOV CX,X
 MOV DX,Y
 MOV BX,Y
 ADD BX,L
+MOV AH,0CH
+MOV AL,Colour
+
 
 Draw:INt 10h
 INC DX
@@ -16,12 +17,13 @@ ENDM DrawVerticalLine
 
 DrawHorizontalLine MACRO X,Y,L,Colour
 LOCAL Draw
-MOV AH,0CH
-MOV AL,Colour
 MOV CX,X
 MOV DX,Y
 MOV BX,X
 ADD BX,L
+
+MOV AH,0CH
+MOV AL,Colour
 
 Draw:INt 10h
 INC CX
@@ -30,17 +32,38 @@ JNZ Draw
 
 ENDM DrawHorizontalLine
 
+DrawTank MACRO 
+    
+    DrawVerticalLine X_Posi1,Y_posi,25D,02h
+    MOV AX,X_Posi1
+    ADD AX,25D
+    DrawVerticalLine AX,Y_posi,25D,02h
+    DrawHorizontalLine X_Posi1,Y_posi,25D,02h
+    MOV AX,Y_Posi
+    ADD AX,25D
+    DrawHorizontalLine X_Posi1,AX,26D,02h
+
+
+
+
+ENDM DrawTank
+
 .MODEL Medium
 .STACK 64
 .DATA
+Tank1 LAbel Byte
+X_Posi1 dw 12
+Y_posi dw 12
+Helath db 3
 .code
 MAIN proc FAR
-
+mov ax,@data
+mov Ds,ax
 MOV AH,0
 MOV AL,13H
 int 10H
 
-DrawHorizontalLine 15,20,200,02h
+DrawTank
 
 ;DrawVerticalLine 16,0,200,03h
 hlt
