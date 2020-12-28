2f4b49d8b1593adc51340b2e58ce08377b942838
moza-loza <61754531+moza-loza@users.noreply.github.com>
2020-12-28 20:02:01 +0200

    update macro to proc

--- Tank_Wars.asm
+++ Tank_Wars.asm
@@ -14,6 +14,8 @@ CMP DX,BX
 JNZ Draw
 ENDM DrawVerticalLine
 
+
+
 DrawHorizontalLine MACRO X,Y,L,Colour ;Darws a single horizontal line from x,y to x+l,z
 LOCAL Draw
 MOV CX,X
@@ -55,86 +57,9 @@ DrawRectangel MACRO X,Y,W,L,Colour
     DrawHorizontalLine X,DX,LengthRec,Colour
 ENDM DrawRectangel
 
-DrawTank MACRO 
-    ;Cx Holds the x values for drawing
-    ;Dx Holds the y values for drawing
-    DrawSquare X_Posi1,Y_Posi,28D,02h
-    MOV AX,28d
-    MOV BX,04D
-    DIV BL
-    MOV AH,0
-    MOV DI,Y_Posi
-    ADD DI,AX
-    MOV SI,X_posi1
-    ADD SI,AX
-    DrawSquare SI,DI,14D,02H
-    
-    MOV AX,28d
-    MOV BX,02D
-    DIV BL
-    MOV AH,0
-    MOV DI,Y_Posi
-    ADD DI,AX
-    MOV SI,X_posi1
-    ADD SI,AX
-    DEC DI
-    DrawRectangel SI,DI,3,28D,07H
 
 
-ENDM DrawTank
 
-Move_Tank1 MACRO
-        MOV AH,1
-        INT 16h
-        CMP AH,48H
-        JZ Move_up
-        CMP AH,50H
-        JZ Move_Down
-        CMP AH,4BH
-        JZ Move_Left
-        CMP AH,4DH
-        JZ Move_Right
-
-        JMP No_Movement
-Move_up:
-        MOV AX,Y_Posi
-        CMP AX,0
-        JZ Read_Value
-        DEC AX
-        MOV Y_Posi,AX
-        JMP Read_Value
-Move_Down:
-        MOV AX,Y_Posi
-        CMP AX,171
-        JZ Read_Value
-        INC AX
-        MOV Y_Posi,AX
-        JMP Read_Value
-Move_Left:
-        MOV AX,X_posi1
-        CMP AX,0
-        JZ Read_Value
-        DEC AX
-        MOV X_posi1,AX
-        JMP Read_Value
-Move_Right:
-        MOV AX,X_posi1
-        CMP AX,277
-        JZ Read_Value
-        INC AX
-        MOV X_posi1,AX
-        JMP Read_Value
-Read_Value:
-        RemoveValueBuffer
-        mov al,0
-	    mov CX,00
-	    mov DX,0FFFFh
-	    mov ah,6
-	    mov bh,0Eh 
-	    int 10h
-No_Movement:nop
-
-ENDM Move_Tank1
 
 RemoveValueBuffer MACRO
     LOCAL NO_value
@@ -147,25 +72,6 @@ RemoveValueBuffer MACRO
         
         
 ENDM RemoveValueBuffer
-
-DrawObstacles Macro
-LOCAL Draw
-LOCAL Increment
-        MOV SI, OFFSET Obstacles
-        MOV DI,[SI]
-        ADD SI,2
-Draw :
-        MOV AX,[SI]+8
-        CMP AX,0
-        JZ Increment
-        DrawRectangel [SI],[SI]+2,[SI]+4,[SI]+6,01
-Increment:
-        ADD SI , 10D
-        DEC DI
-        JNZ Draw 
-
-ENDM DrawObstacles
-
 
 collisionDetection Macro xt,xo,Lt1,Lo
         LOCAL secondCheck
@@ -205,6 +111,7 @@ ENDM collisionDetection
 	        Tank1 Label Byte
 	X_Posi1 dw    100
 	Y_posi  dw    100
+        Tank_length dw    28d
 	x       dw    ?
 	y       dw    ?
         yObs    dw    ?
@@ -214,6 +121,175 @@ ENDM collisionDetection
 Obstacles DW 2,50,50,10,5,1,20,50,10,5,1 ;nObstacles, x , y , width, length of obstacles , DrawStatus 1: to be drawn 0: Destroyed
 LengthRec DW ?
 .code
+delay       proc 
+            mov     cx, 002H
+         delRep: push    cx
+            mov     cx, 0D090H
+         delDec: dec     cx
+            jnz     delDec
+            pop     cx
+            dec     cx
+            jnz     delRep
+            ret
+delay       endp
+
+Move_Tank1 proc
+                MOV AH,1
+                INT 16h
+                CMP AH,48H
+                JZ Move_up
+                CMP AH,50H
+                JZ Move_Down
+                CMP AH,4BH
+                JZ Move_Left
+                CMP AH,4DH
+                JZ Move_Right
+
+                JMP No_Movement
+        Move_up:
+                MOV AX,Y_Posi
+                CMP AX,0
+                JZ Read_Value
+                DEC AX
+                MOV Y_Posi,AX
+                JMP Read_Value
+        Move_Down:
+                MOV AX,Y_Posi
+                CMP AX,171
+                JZ Read_Value
+                INC AX
+                MOV Y_Posi,AX
+                JMP Read_Value
+        Move_Left:
+                MOV AX,X_posi1
+                CMP AX,0
+                JZ Read_Value
+                DEC AX
+                MOV X_posi1,AX
+                JMP Read_Value
+        Move_Right:
+                MOV AX,X_posi1
+                CMP AX,277
+                JZ Read_Value
+                INC AX
+                MOV X_posi1,AX
+                JMP Read_Value
+        Read_Value:
+                RemoveValueBuffer
+                mov al,0
+                mov CX,00
+                mov DX,0FFFFh
+                mov ah,6
+                mov bh,0Eh 
+                int 10h
+        No_Movement:    ret
+Move_Tank1 ENDp
+
+DrawTank1 proc 
+                       mov                si,2
+	               MOv                Dx,Y_posi
+	firstloop:     
+	               MOV                CX,X_Posi1
+	               inc                CX
+	               DrawHorizontalLine CX,DX,26D,00
+	               MOV                CX,X_Posi1
+	               inc                CX
+	               add                dx ,Tank_length
+	               DrawHorizontalLine CX,DX,26D,00
+	               sub                dx ,Tank_length
+	               mov                ax,Tank_length
+	               sub                ax,2
+	               mov                Tank_length,ax
+	               inc                dx
+	               dec                SI
+	               jnz                firstloop
+
+	               dec                dx
+
+	               mov                si,4
+	secondloop:    
+	               mov                Cx,X_Posi1
+	               inc                dx
+	               DrawHorizontalLine CX,DX,1,00
+	               DrawHorizontalLine CX,DX,26,01
+	               DrawHorizontalLine CX,DX,1,00
+	               mov                Cx,X_Posi1
+	               add                dx,Tank_length
+	               DrawHorizontalLine CX,DX,1,00
+	               DrawHorizontalLine CX,DX,26,01
+	               DrawHorizontalLine CX,DX,1,00
+	               sub                dx ,Tank_length
+	               mov                ax,Tank_length
+	               sub                ax,2
+	               mov                Tank_length,ax
+	               dec                SI
+	               jz                 beginthirdloop
+	               jmp                secondloop
+
+	beginthirdloop:mov                si,2
+	thirdloop:     
+	               mov                cx,X_Posi1
+	               inc                dx
+	               DrawHorizontalLine CX,DX,28,00
+	               mov                cx,X_Posi1
+	               add                dx,Tank_length
+	               DrawHorizontalLine CX,DX,28,00
+	               sub                dx,Tank_length
+	               mov                ax,Tank_length
+	               sub                ax,2
+	               mov                Tank_length,ax
+	               dec                SI
+	               jnz                thirdloop
+
+	               mov                si,4
+	fourthloop:    
+	               mov                cx,X_Posi1
+	               inc                dx
+	               inc                cx
+	               DrawHorizontalLine cx,dx,5,00
+	               DrawHorizontalLine cx,dx,15,01
+	               DrawHorizontalLine cx,dx,3,00
+	               mov                cx,X_Posi1
+	               add                dx,Tank_length
+	               inc                cx
+	               DrawHorizontalLine cx,dx,5,00
+	               DrawHorizontalLine cx,dx,15,01
+	               DrawHorizontalLine cx,dx,3,00
+	               sub                dx,Tank_length
+	               mov                ax,Tank_length
+	               sub                ax,2
+	               mov                Tank_length,ax
+	               dec                SI
+	               jz                 beginfifthloop
+	               jmp                fourthloop
+
+	beginfifthloop:mov                si , 5
+	fifthloop:     mov                cx,X_Posi1
+	               inc                dx
+	               add                cx,2
+	               DrawHorizontalLine cx,dx,28,00
+	               dec                SI
+	               jnz                fifthloop
+        mov Tank_length,28d
+        ret
+DrawTank1 endp
+
+DrawObstacles proc
+
+                MOV SI, OFFSET Obstacles
+                MOV DI,[SI]
+                ADD SI,2
+        Draw :
+                MOV AX,[SI]+8
+                CMP AX,0
+                JZ Increment
+                DrawRectangel [SI],[SI]+2,[SI]+4,[SI]+6,01
+        Increment:
+                ADD SI , 10D
+                DEC DI
+                JNZ Draw 
+                ret
+DrawObstacles endp
 MAIN proc FAR
 
 	          mov           ax,@data
@@ -229,39 +305,22 @@ MAIN proc FAR
 	          mov           bh,0Eh
 	          int           10h
 
-                mov si,OFFSET Obstacles
-                add si,2
-                mov AX,[si]
-                mov xObs,AX
-                mov AX,[si]+2
-                mov yObs,AX
-                mov AX,[si]+4
-                mov wObs,AX
-                mov AX,[si]+6
-                mov lObs,AX
-                
-               
-	labeltest:
-                
-                DrawObstacles        
-	        DrawTank
-	        Move_Tank1
-                collisionDetection X_Posi1,xObs,28D,lobs
-                MOV bl,al
-                collisionDetection Y_posi,yObs,28D,wobs
-                and al,bl 
-                cmp al,1
-                jz overwrite
-                       
-                jmp labeltest      
-               overwrite: 
-               mov si,OFFSET Obstacles
-                add si,2 
 
-                mov ax,0
-                mov [si]+8,ax
-                RemoveValueBuffer 
-	          jmp           labeltest
+                      
+	labeltest:
+                Call Move_Tank1
+                Call DrawObstacles
+                Call DrawTank1
+	        
+                
+
+                ;process input : update tank coordinates , health , score , bullets
+                ;update : change data , collision
+                ; render : draw 
+
+                        
+                jmp labeltest      
+
 	          mov           AH,0Ch
 	          int           21h
                   
